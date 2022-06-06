66ad635e9a0f2537e8a5b0bd69220f9b1f8cf583
Sandrine <sandrine.thominet@gmail.com>
2022-06-05 20:04:15 -0400

    Completed assignment

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,10 +2,7 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>
-    <a href="#home">Home</a>
-    <a href="#about">About</a>
-  </nav>;
+  return <nav>NavBar</nav>;
 }
 
 export default NavBar;
